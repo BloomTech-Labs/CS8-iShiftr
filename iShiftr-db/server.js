21cb019eb961e8a5053b1114355330fc5da787ed
habib1234731 <habibrehman1234731@gmail.com>
2018-07-18 12:36:47 -0400

    moved to mongo, initialized that backend

---
+++ iShiftr-db/server.js
@@ -0,0 +1,19 @@
+const express     = require('express');
+const cors        = require('cors');
+const helmet      = require('helmet');
+const morgan      = require('morgan');
+const routes      = require('./routes/routes');
+const server      = express();
+
+const corsOptions = {
+    origin:'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}
+
+server.use(helmet());
+server.use(express.json());
+server.use(morgan('dev'));
+server.use(cors(corsOptions));
+routes(server);
+module.exports    = server;
