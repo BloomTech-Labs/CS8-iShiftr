e27921c7907f5c0353de9e16e72dde5f64f90c08
Cristina Laryea <crispad06@gmail.com>
2018-07-20 13:20:13 -0400

    fixed cors

--- iShiftr-db/server.js
+++ iShiftr-db/server.js
@@ -5,12 +5,20 @@ const morgan      = require('morgan');
 const routes      = require('./routes/routes');
 const server      = express();
 
+// const corsOptions = {
+//     origin: ['https://ishiftr.netlify.com'],
+//     // origin:'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true
+// }
+
 const corsOptions = {
-    origin: ['https://ishiftr.netlify.com'],
-    // origin:'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}
+    origin: ('http://localhost:3000', 'https://ishiftr.netlify.com'),
+      credentials: true,
+      methods: ['GET', 'PUT', 'POST'],
+      allowedHeaders: ['Content-Type', 'Authorization']
+  }
+  
 
 server.use(cors(corsOptions));
 server.use(helmet());
