815d4ce52902b39d07249ec8690f53131390a23b
habib1234731 <habibrehman1234731@gmail.com>
2018-07-20 18:14:40 -0400

    changed the mongo url and secret in app and login

--- iShiftr-db/app.js
+++ iShiftr-db/app.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const server   = require('./server');
 const port     = process.env.PORT || 5000;
 
+// .connect('mongodb://localhost/iShiftr')
 mongoose
-    // connect(process.env.MONGO_URI)
-    .connect('mongodb://localhost/iShiftr')
+connect(process.env.MONGO_URI)
     .then(()=> {
         console.log('\n\n--Connected to the Database Successfully--\n\n');
     })
