c43dcc4eb589c6641ddb8bc02a554d1c7d1f14af
astro11 <aquila.strother@asu.edu>
2018-07-20 11:29:49 -0400

    added handleInputChange

--- ishiftr-frontend/src/Components/Signup.js
+++ ishiftr-frontend/src/Components/Signup.js
@@ -9,35 +9,47 @@ class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: '',            
-            username: '',
-            password: '',
-            firstName: '',
-            lastName: '',
-            phoneNumber: ''
-
+            formData: {
+                email: '',            
+                username: '',
+                password: '',
+                firstName: '',
+                lastName: '',
+                phoneNumber: ''
+            }
         }
         this.signUpHandler = this.signUpHandler.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
     // TODO: create Admin Sign up --first-- BackLog Employee SignUp
 
     // config: { headers: {'Content-Type': 'multipart/form-data' }}
+    handleInputChange(e) {
+        let formData = {...this.state.formData};
+        formData[e.target.name] = e.target.value;
+        this.setState({ 
+            formData 
+        });
+        console.log(this.state);
+    }
 
-    signUpHandler(e) {
+
+    signUpHandler(e, formData) {
         e.preventDefault();        
-        axios.post('https://ishiftr-db.herokuapp.com/', {
-            username: this.state.username,
-            password: this.state.password,
-            email: this.state.email,
-            firstName: this.state.firstName,
-            lastName: this.state.lastName,
-            phoneNumber: this.state.phoneNumber
+        axios.post('https://ishiftr-db.herokuapp.com/', formData, {
+            // headers: {'Access-Control-Allow-Origin': '*'}            
+            // username: this.state.username,
+            // password: this.state.password,
+            // email: this.state.email,
+            // firstName: this.state.firstName,
+            // lastName: this.state.lastName,
+            // phoneNumber: this.state.phoneNumber
         })
         .then(function (res) {
             console.log(res);
         })
         .catch(function (error) {
-            console.log(error);
+            console.log('there is an error signing up', error);
         });
     }
 
@@ -52,37 +64,37 @@ class SignUp extends Component {
                     <FormGroup row>
                         <Label sm ={4} for="username">Username:</Label>
                         <Col sm ={8}>
-                            <Input type="text" name="username" id="#username" placeholder="Choose a username" />
+                            <Input onChange={this.handleInputChange} value={this.state.username} type="text" name="username" id="#username" placeholder="Choose a username" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label sm ={4} for="firstname">First Name:</Label>
                         <Col sm ={8}>
-                            <Input type="text" name="firstName" id="#firstname" placeholder="First Name" />
+                            <Input  onChange={this.handleInputChange}  value={this.state.firstName} type="text" name="firstName" id="#firstname" placeholder="First Name" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label sm ={4} for="lastname">Last Name:</Label>
                         <Col sm ={8}>
-                            <Input type="text" name="lastName" id="#lastname" placeholder="Last Name" />
+                            <Input onChange={this.handleInputChange} value={this.state.lastName} type="text" name="lastName" id="#lastname" placeholder="Last Name" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label sm ={4} for="email">Email:</Label>
                         <Col sm ={8}>
-                            <Input type="email" name="email" id="#email" placeholder="Enter Your Email" />
+                            <Input onChange={this.handleInputChange}  value={this.state.email}type="email" name="email" id="#email" placeholder="Enter Your Email" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label sm = {4}for="password">Password:</Label>
                         <Col sm ={8}>
-                            <Input type="password" name="password" id="#password" placeholder="Enter a password" />
+                            <Input onChange={this.handleInputChange} value={this.state.password} type="password" name="password" id="#password" placeholder="Enter a password" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
                         <Label sm = {4}for="re-password">Phone Number:</Label>
                         <Col sm ={8}>
-                            <Input type="tel" data-country="US" name="phoneNumber" id="#phonenumber" placeholder="Area Code First" />
+                            <Input onChange={this.handleInputChange} type="tel" data-country="US" name="phoneNumber" id="#phonenumber" placeholder="Area Code First" />
                         </Col>
                     </FormGroup>
                     <Button color = "primary" type="submit">Register</Button> <br />
