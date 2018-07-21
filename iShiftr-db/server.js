827e2bf7055100643ae31f2b5d7dc33a7443c88f
habib1234731 <habibrehman1234731@gmail.com>
2018-07-20 21:45:45 -0400

    changed the origin in corsOptions

--- iShiftr-db/server.js
+++ iShiftr-db/server.js
@@ -6,14 +6,14 @@ const routes      = require('./routes/routes');
 const server      = express();
 
 const corsOptions = {
-    origin: "*",
+    origin: 'https://ishiftr-db.herokuapp.com/',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders:['Content-Type', 'Authorization'],
     credentials: true
 }
 
-server.use(cors(corsOptions));
 server.use(helmet());
+server.use(cors(corsOptions));
 server.use(express.json());
 server.use(morgan('dev'));
 routes(server);
