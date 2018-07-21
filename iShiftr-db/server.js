67826c0ec793c463e1aacb2fc7cd0130be526fdc
habib1234731 <habibrehman1234731@gmail.com>
2018-07-20 21:49:04 -0400

    fixed the origin in corsOptions

--- iShiftr-db/server.js
+++ iShiftr-db/server.js
@@ -6,7 +6,7 @@ const routes      = require('./routes/routes');
 const server      = express();
 
 const corsOptions = {
-    origin: 'https://ishiftr-db.herokuapp.com/',
+    origin: 'https://ishiftr.netlify.com/',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders:['Content-Type', 'Authorization'],
     credentials: true
