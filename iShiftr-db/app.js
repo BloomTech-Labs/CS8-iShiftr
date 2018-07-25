2bdc6e9ad399bf9a9129f1ea8a5293061a5bdf4a
habib1234731 <habibrehman1234731@gmail.com>
2018-07-25 19:47:40 -0400

    changed the mongodb route and added a method to make paid true if Employer pays

--- iShiftr-db/app.js
+++ iShiftr-db/app.js
@@ -3,9 +3,9 @@ const server   = require('./server');
 const port     = process.env.PORT || 5000;
 require("dotenv").config();
 
+// .connect('mongodb://localhost/iShiftr')
 mongoose
-.connect('mongodb://localhost/iShiftr')
-    // .connect(process.env.MONGODB_URI)
+    .connect(process.env.MONGODB_URI)
     .then(()=> {
         console.log('\n\n--Connected to the Database Successfully--\n\n');
     })
--- iShiftr-db/controllers/payment.js
+++ iShiftr-db/controllers/payment.js
@@ -1,5 +1,5 @@
 const stripe = require('../models/PaymentModel');
-
+const Employer = require('../models/EmployerModel');
 const postStripeCharge = res => (stripeErr, stripeRes) => {
   if (stripeErr) {
     res.status(500).send({ error: stripeErr });
@@ -10,12 +10,22 @@ const postStripeCharge = res => (stripeErr, stripeRes) => {
 
 const paymentApi = async (req, res) => {
   try {
-    let {status} = await stripe.charges.create({
+    let { status } = await stripe.charges.create({
       amount: req.body.amount,
       currency: req.body.currency,
       description: req.body.description,
       source: req.body
     });
+    Employer
+      .findByIdAndUpdate(req.body.id, {
+        $set: {paid: true}
+      })
+      .then(employer => {
+        res.status(200).json({ employer })
+      })
+      .catch((error) => {
+        res.status(500).json({ Error: 'There was an error getting the employer', error })
+      })
   }
   catch (error) {
     res.status(500).end();
@@ -23,5 +33,5 @@ const paymentApi = async (req, res) => {
 };
 
 module.exports = {
-    paymentApi
+  paymentApi
 };
